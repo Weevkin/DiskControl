097442cd9bc63fbfcbd20ad0d5b4f07d9e33eeba
Weevkin <weevilmonkey@gmail.com>
2012-09-20 22:48:37 -0600

    Number 2

--- src/com/github/weevkin/diskcontrol/DiskControlListener.java
+++ src/com/github/weevkin/diskcontrol/DiskControlListener.java
@@ -1,10 +1,12 @@
 package com.github.weevkin.diskcontrol;
 
-import org.bukkit.Server;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 
 
@@ -16,21 +18,18 @@ public class DiskControlListener implements Listener{
 		Player p = event.getPlayer();
 		String picked = i.getItemStack().getType().toString();
 		if(picked == "RECORD_9"){
-			p.sendMessage(p.getDisplayName()+ " picked up the record!");
+			Bukkit.broadcastMessage(p.getDisplayName() + " has picked up the record!");
 			p.getPlayer().getWorld().strikeLightning(p.getPlayer().getTargetBlock(null, 200).getLocation());
 			
 		}
-		else{
-			p.sendMessage(i.getItemStack().getType().toString().toLowerCase());
-			p.sendMessage("no biggy, just a " + i.getItemStack().getType().toString().toLowerCase().replace("_", " "));
-		}
-		
-		//p.sendMessage(p.getDisplayName()+ " picked up a(n) " + i.getItemStack().getType().toString().toLowerCase().replace("_", " "));
 	}
-	/*public void onBlockPlace(BlockPlaceEvent event){
+	public void onJukeboxUse(PlayerInteractEvent event){
 		Player p = event.getPlayer();
-		Block bp = event.getBlockPlaced();
-		
-		p.sendMessage("You placed a " + bp.getType().toString());
-	}*/
+		Action a = event.getAction();
+		p.sendMessage("fu");
+		//if(a.equals(Action.RIGHT_CLICK_BLOCK)){
+			p.sendMessage(" Interacted with:" + event.getEventName().toString());
+		//}
+		p.sendMessage(" Interacted with:" + event.getEventName().toString());
+	}
 }
