3c97594ed6048e972efccc17f950102c77575ad0
Weevkin <weevilmonkey@gmail.com>
2012-09-20 23:29:49 -0600

    Plugin stage 1!

--- src/com/github/weevkin/diskcontrol/DiskControlListener.java
+++ src/com/github/weevkin/diskcontrol/DiskControlListener.java
@@ -1,11 +1,15 @@
 package com.github.weevkin.diskcontrol;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Effect;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 
@@ -23,13 +27,28 @@ public class DiskControlListener implements Listener{
 			
 		}
 	}
+	@EventHandler
 	public void onJukeboxUse(PlayerInteractEvent event){
 		Player p = event.getPlayer();
 		Action a = event.getAction();
-		p.sendMessage("fu");
-		//if(a.equals(Action.RIGHT_CLICK_BLOCK)){
-			p.sendMessage(" Interacted with:" + event.getEventName().toString());
-		//}
-		p.sendMessage(" Interacted with:" + event.getEventName().toString());
+		Block b = event.getClickedBlock();
+		String inhand = p.getItemInHand().getType().toString();
+		
+		if(a.equals(Action.RIGHT_CLICK_BLOCK) && b.getType().equals(Material.JUKEBOX) && inhand.equals("RECORD_9")){
+			event.getPlayer().getWorld().playEffect(p.getLocation(), Effect.ENDER_SIGNAL, 0);
+			event.getPlayer().getWorld().createExplosion(p.getLocation(), 0);
+			event.getPlayer().getWorld().playEffect(p.getLocation(), Effect.SMOKE, 0);
+			Bukkit.broadcastMessage(p.getDisplayName() + " has placed the Disk into a jukebox!");
+		}
+	}
+	@EventHandler
+	public void onItemDrop(PlayerDropItemEvent event){
+		Player p = event.getPlayer();
+		Item i = event.getItemDrop();
+		
+		String dropped = i.getItemStack().getType().toString();
+		if(dropped.equals("RECORD_9")){
+			Bukkit.broadcastMessage(p.getDisplayName() + " has dropped at X: " + i.getLocation().getBlockX() + " Y: " + i.getLocation().getBlockY() + " Z: " + i.getLocation().getBlockZ());
+		}
 	}
 }
