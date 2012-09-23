476837cb0c7243b1467b4aa91d924dcf753492a9
Weevkin <weevilmonkey@gmail.com>
2012-09-23 15:52:58 -0600

    Using new classes for mysql.

--- src/com/github/weevkin/diskcontrol/DiskControl.java
+++ src/com/github/weevkin/diskcontrol/DiskControl.java
@@ -1,10 +1,95 @@
 package com.github.weevkin.diskcontrol;
 
+import java.io.File;
+import java.util.logging.Logger;
+
+import lib.PatPeter.SQLibrary.MySQL;
+
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DiskControl extends JavaPlugin{
+	public String logPrefix = "[DiskControl] "; // Prefix to go in front of all log entries
+	public Logger log = Logger.getLogger("Minecraft"); // Minecraft log and console
+	public File pFolder = new File("plugins" + File.separator + "DiskControl"); // Folder to store plugin settings file and database
+	
+	public MySQL mysql;
+	
+	public boolean MySQL = false;
+	public String dbHost = "localhost";
+	public String dbPort = "3306";
+	public String dbUser = "root";
+	public String dbPass = "";
+	public String dbDatabase = "diskcontrol";
+	
 	@Override
     public void onEnable(){
 		getServer().getPluginManager().registerEvents(new DiskControlListener(), this);
+		Bukkit.broadcastMessage("Testing");
+		this.log.warning("This is a test");
+		//SQL Stuff
+		if (this.MySQL) {
+			if (this.dbHost.equals(null)) {
+				this.MySQL = false;
+				this.log.severe(this.logPrefix + "MySQL is on, but host is not defined, defaulting to SQLite");
+			}
+			if (this.dbUser.equals(null)) {
+				this.MySQL = false;
+				this.log.severe(this.logPrefix + "MySQL is on, but username is not defined, defaulting to SQLite");
+			}
+			if (this.dbPass.equals(null)) {
+				this.MySQL = false;
+				this.log.severe(this.logPrefix + "MySQL is on, but password is not defined, defaulting to SQLite");
+			}
+			if (this.dbDatabase.equals(null)) {
+				this.MySQL = false;
+				this.log.severe(this.logPrefix + "MySQL is on, but database is not defined, defaulting to SQLite");
+			}
+		}
+		// Enabled SQL/MySQL
+				if (this.MySQL) {
+					// Declare MySQL Handler
+					this.mysql = new MySQL(this.log, this.logPrefix, this.dbHost, this.dbPort, this.dbDatabase, this.dbUser, this.dbPass);
+					this.log.info(this.logPrefix + "MySQL Initializing");
+					// Initialize MySQL Handler
+					try {
+						this.mysql.open();
+					} catch (Exception e) {
+						Bukkit.broadcastMessage(e.getMessage());
+					}
+					
+					//try {
+						if (this.mysql.checkConnection()) { // Check if the Connection was successful
+							this.log.info(this.logPrefix + "MySQL connection successful");
+							if (!this.mysql.checkTable("blocks")) { // Check if the table exists in the database if not create it
+								this.log.info(this.logPrefix + "Creating table blocks");
+								String query = "CREATE TABLE blocks (id INT, owner VARCHAR(255), x INT, y INT, z INT);";
+								this.mysql.createTable(query);
+							}
+						} else {
+							this.log.severe(this.logPrefix + "MySQL connection failed");
+							this.MySQL = false;
+						}
+					/*} catch (MalformedURLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (InstantiationException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IllegalAccessException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}*/
+				}
+				else{
+					Bukkit.broadcastMessage("MYSQL Failed to load.");
+					Bukkit.broadcastMessage(this.logPrefix+" / "+this.dbHost+" / "+this.dbPort+" / "+this.dbDatabase+" / "+this.dbUser+" / "+this.dbPass);
+				}
+				
+				// Register Listeners \\
+				//PluginManager pm = this.getServer().getPluginManager();
+				//pm.registerEvent(, new OwnerPlayerListener(this), EventPriority.NORMAL, new SQLEventExecutor(), this);
+				
+				this.log.info(this.logPrefix + "Owner is finished initializing");
     }
 }
--- src/com/github/weevkin/diskcontrol/DiskControlListener.java
+++ src/com/github/weevkin/diskcontrol/DiskControlListener.java
@@ -1,8 +1,10 @@
 package com.github.weevkin.diskcontrol;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Effect;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
@@ -13,42 +15,67 @@ import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 
-
 public class DiskControlListener implements Listener{
-
+	private DiskControl plugin;
+	
+	public DiskControlListener() {
+		
+	}
+	
+	public DiskControlListener(DiskControl plugin) {
+		this.plugin = plugin;
+	}
+	
 	@EventHandler
 	public void onItemPickup(PlayerPickupItemEvent event){
 		Item i = event.getItem();
 		Player p = event.getPlayer();
 		String picked = i.getItemStack().getType().toString();
 		if(picked == "RECORD_9"){
-			Bukkit.broadcastMessage(p.getDisplayName() + " has picked up the record!");
-			p.getPlayer().getWorld().strikeLightning(p.getPlayer().getTargetBlock(null, 200).getLocation());
-			
+			Bukkit.broadcastMessage(p.getDisplayName()+ " picked up the record!");
+			p.getPlayer().getWorld().strikeLightningEffect(p.getPlayer().getTargetBlock(null, 200).getLocation());
 		}
 	}
+	
+	@EventHandler
+	public void onItemDrop(PlayerDropItemEvent event){
+		Item i = event.getItemDrop();
+		Player p = event.getPlayer();
+		
+		String dropped = i.getItemStack().getType().toString();
+		if(dropped.equals("RECORD_9")){
+			Bukkit.broadcastMessage(p.getDisplayName()+ " dropped the record at X: "+i.getLocation().getBlockX()+" Y: "+i.getLocation().getBlockY()+" Z: "+i.getLocation().getBlockZ());
+		}
+	}
+	
 	@EventHandler
 	public void onJukeboxUse(PlayerInteractEvent event){
 		Player p = event.getPlayer();
 		Action a = event.getAction();
 		Block b = event.getClickedBlock();
-		String inhand = p.getItemInHand().getType().toString();
 		
-		if(a.equals(Action.RIGHT_CLICK_BLOCK) && b.getType().equals(Material.JUKEBOX) && inhand.equals("RECORD_9")){
-			event.getPlayer().getWorld().playEffect(p.getLocation(), Effect.ENDER_SIGNAL, 0);
-			event.getPlayer().getWorld().createExplosion(p.getLocation(), 0);
+		if(a.equals(Action.RIGHT_CLICK_BLOCK) && b.getType().toString().equals("JUKEBOX") && p.getItemInHand().getType().toString().equals("RECORD_9")){
+			//Check to see if item placed as part of interaction was a RECORD_9
+			//World..getWorld().playEffect(p.getLocation(),"",1);
+			p.getPlayer().getWorld().createExplosion(p.getLocation(), 0);
 			event.getPlayer().getWorld().playEffect(p.getLocation(), Effect.SMOKE, 0);
-			Bukkit.broadcastMessage(p.getDisplayName() + " has placed the Disk into a jukebox!");
-		}
-	}
-	@EventHandler
-	public void onItemDrop(PlayerDropItemEvent event){
-		Player p = event.getPlayer();
-		Item i = event.getItemDrop();
-		
-		String dropped = i.getItemStack().getType().toString();
-		if(dropped.equals("RECORD_9")){
-			Bukkit.broadcastMessage(p.getDisplayName() + " has dropped at X: " + i.getLocation().getBlockX() + " Y: " + i.getLocation().getBlockY() + " Z: " + i.getLocation().getBlockZ());
+			event.getPlayer().getWorld().playEffect(p.getLocation(), Effect.ENDER_SIGNAL, 0);
+			Bukkit.broadcastMessage(p.getDisplayName() + " has placed the Disk into a Jukebox");
+			
+			String query = "INSERT INTO capture_info('player', 'date') VALUES('"+p.getDisplayName()+"','NOW()');";
+			ResultSet result = null;
+			
+			if (plugin.MySQL) {
+				try {
+					result = this.plugin.mysql.query(query);
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			if (result != null){
+				p.sendMessage("Query sent");
+			}
 		}
 	}
 }
