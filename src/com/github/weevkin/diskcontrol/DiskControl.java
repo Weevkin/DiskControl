e6275c91cafbea7bcfa317a56e8f56d45e0cb54e
Weevkin <weevilmonkey@gmail.com>
2012-09-23 16:00:21 -0600

    Updated Stuff!

--- src/com/github/weevkin/diskcontrol/DiskControl.java
+++ src/com/github/weevkin/diskcontrol/DiskControl.java
@@ -15,7 +15,7 @@ public class DiskControl extends JavaPlugin{
 	
 	public MySQL mysql;
 	
-	public boolean MySQL = false;
+	public boolean MySQL = true;
 	public String dbHost = "localhost";
 	public String dbPort = "3306";
 	public String dbUser = "root";
@@ -45,51 +45,49 @@ public class DiskControl extends JavaPlugin{
 				this.MySQL = false;
 				this.log.severe(this.logPrefix + "MySQL is on, but database is not defined, defaulting to SQLite");
 			}
-		}
-		// Enabled SQL/MySQL
-				if (this.MySQL) {
-					// Declare MySQL Handler
-					this.mysql = new MySQL(this.log, this.logPrefix, this.dbHost, this.dbPort, this.dbDatabase, this.dbUser, this.dbPass);
-					this.log.info(this.logPrefix + "MySQL Initializing");
-					// Initialize MySQL Handler
-					try {
-						this.mysql.open();
-					} catch (Exception e) {
-						Bukkit.broadcastMessage(e.getMessage());
+		
+			// Declare MySQL Handler
+			this.mysql = new MySQL(this.log, this.logPrefix, this.dbHost, this.dbPort, this.dbDatabase, this.dbUser, this.dbPass);
+			this.log.info(this.logPrefix + "MySQL Initializing");
+			// Initialize MySQL Handler
+			try {
+				this.mysql.open();
+			} catch (Exception e) {
+				Bukkit.broadcastMessage(e.getMessage());
+			}
+			
+			//try {
+				if (this.mysql.checkConnection()) { // Check if the Connection was successful
+					this.log.info(this.logPrefix + "MySQL connection successful");
+					if (!this.mysql.checkTable("blocks")) { // Check if the table exists in the database if not create it
+						this.log.info(this.logPrefix + "Creating table blocks");
+						String query = "CREATE TABLE blocks (id INT, owner VARCHAR(255), x INT, y INT, z INT);";
+						this.mysql.createTable(query);
 					}
-					
-					//try {
-						if (this.mysql.checkConnection()) { // Check if the Connection was successful
-							this.log.info(this.logPrefix + "MySQL connection successful");
-							if (!this.mysql.checkTable("blocks")) { // Check if the table exists in the database if not create it
-								this.log.info(this.logPrefix + "Creating table blocks");
-								String query = "CREATE TABLE blocks (id INT, owner VARCHAR(255), x INT, y INT, z INT);";
-								this.mysql.createTable(query);
-							}
-						} else {
-							this.log.severe(this.logPrefix + "MySQL connection failed");
-							this.MySQL = false;
-						}
-					/*} catch (MalformedURLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (InstantiationException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IllegalAccessException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}*/
+				} else {
+					this.log.severe(this.logPrefix + "MySQL connection failed");
+					this.MySQL = false;
 				}
-				else{
-					Bukkit.broadcastMessage("MYSQL Failed to load.");
-					Bukkit.broadcastMessage(this.logPrefix+" / "+this.dbHost+" / "+this.dbPort+" / "+this.dbDatabase+" / "+this.dbUser+" / "+this.dbPass);
-				}
-				
-				// Register Listeners \\
-				//PluginManager pm = this.getServer().getPluginManager();
-				//pm.registerEvent(, new OwnerPlayerListener(this), EventPriority.NORMAL, new SQLEventExecutor(), this);
-				
-				this.log.info(this.logPrefix + "Owner is finished initializing");
+			/*} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}*/
+		}
+		else{
+			Bukkit.broadcastMessage("MYSQL Failed to load.");
+			Bukkit.broadcastMessage(this.logPrefix+" / "+this.dbHost+" / "+this.dbPort+" / "+this.dbDatabase+" / "+this.dbUser+" / "+this.dbPass);
+		}
+		
+		// Register Listeners \\
+		//PluginManager pm = this.getServer().getPluginManager();
+		//pm.registerEvent(, new OwnerPlayerListener(this), EventPriority.NORMAL, new SQLEventExecutor(), this);
+		
+		this.log.info(this.logPrefix + "Owner is finished initializing");
     }
 }
