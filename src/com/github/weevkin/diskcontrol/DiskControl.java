0bae94b98d39a9f62334a3166683af9537bc339f
Weevkin <weevilmonkey@gmail.com>
2012-09-23 22:23:36 -0600

    Stage 2!

--- src/com/github/weevkin/diskcontrol/DiskControl.java
+++ src/com/github/weevkin/diskcontrol/DiskControl.java
@@ -8,6 +8,9 @@ import java.util.logging.Logger;
 import lib.PatPeter.SQLibrary.MySQL;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DiskControl extends JavaPlugin{
@@ -57,31 +60,20 @@ public class DiskControl extends JavaPlugin{
 			}
 			
 			//try {
-				if (this.mysql.checkConnection()) { // Check if the Connection was successful
-					this.log.info(this.logPrefix + "MySQL connection successful");
-					if (!this.mysql.checkTable("blocks")) { // Check if the table exists in the database if not create it
-						this.log.info(this.logPrefix + "Creating table blocks");
-						String query = "CREATE TABLE blocks (id INT, owner VARCHAR(255), x INT, y INT, z INT);";
-						this.mysql.createTable(query);
-					}
-				} else {
-					this.log.severe(this.logPrefix + "MySQL connection failed");
-					this.MySQL = false;
+			if (this.mysql.checkConnection()) { // Check if the Connection was successful
+				this.log.info(this.logPrefix + "MySQL connection successful");
+				if (!this.mysql.checkTable("blocks")) { // Check if the table exists in the database if not create it
+					this.log.info(this.logPrefix + "Creating table blocks");
+					String query = "CREATE TABLE blocks (id INT, owner VARCHAR(255), x INT, y INT, z INT);";
+					this.mysql.createTable(query);
 				}
-			/*} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InstantiationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}*/
+			} else {
+				this.log.severe(this.logPrefix + "MySQL connection failed");
+				this.MySQL = false;
+			}
 		}
 		else{
 			Bukkit.broadcastMessage("MYSQL Failed to load.");
-			Bukkit.broadcastMessage(this.logPrefix+" / "+this.dbHost+" / "+this.dbPort+" / "+this.dbDatabase+" / "+this.dbUser+" / "+this.dbPass);
 		}
 		
 		// Register Listeners \\
@@ -90,6 +82,31 @@ public class DiskControl extends JavaPlugin{
 		
 		this.log.info(this.logPrefix + "Owner is finished initializing");
     }
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		Player p = (Player)sender;
+		
+		if (commandLabel.equalsIgnoreCase("disk")) {
+			String query = "SELECT `id`, `player`, DATE_FORMAT(`date`, '%W') as `date`, DATE_FORMAT(`date`, '%l:%i %p') as `hm` FROM capture_info ORDER BY `id` desc LIMIT 1";
+			ResultSet res = this.runSelect(query);
+			
+			try {
+				if (res != null && res.next()) {
+					String player1 = res.getString("player");
+					String date = res.getString("date");
+					String hm = res.getString("hm");
+					
+					p.sendMessage(player1 + " captured the disk on " + date + " at " + hm);
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return true;
+		}
+		
+		return false;
+	}
 	
 	public ResultSet runInsert(String query){
 		ResultSet result = null;
--- src/com/github/weevkin/diskcontrol/DiskControlListener.java
+++ src/com/github/weevkin/diskcontrol/DiskControlListener.java
@@ -2,18 +2,24 @@ package com.github.weevkin.diskcontrol;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Effect;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class DiskControlListener implements Listener{
 	private DiskControl plugin;
@@ -27,10 +33,22 @@ public class DiskControlListener implements Listener{
 	}
 	
 	@EventHandler
-	public void onPlayerLogin(PlayerJoinEvent event) throws SQLException{
-		String query = "SELECT `id`, `player`, DATE_FORMAT(`date`, '%W') as `date`, DATE_FORMAT(`date`, '%l:%i %p') as `hm` FROM capture_info ORDER BY `id` desc LIMIT 1";
-		//ResultSet result = this.plugin.runQuery(query);
+	public void onDeath(PlayerDeathEvent event){
+		List<ItemStack> i = event.getDrops();
+		Player p = event.getEntity().getPlayer();
 		
+		for (Iterator<ItemStack> j = i.iterator(); j.hasNext(); ) {
+			ItemStack s = j.next();
+			if (s.getType() == Material.RECORD_9){
+				Bukkit.broadcastMessage(p.getDisplayName()+ " dropped the record at X: "+p.getLocation().getBlockX()+" Y: "+p.getLocation().getBlockY()+" Z: "+p.getLocation().getBlockZ());
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerLogin(PlayerJoinEvent event) throws SQLException{
+		Player p = event.getPlayer();
+		String query = "SELECT `id`, `player`, DATE_FORMAT(`date`, '%W') as `date`, DATE_FORMAT(`date`, '%l:%i %p') as `hm` FROM capture_info ORDER BY `id` desc LIMIT 1";
 		ResultSet res = this.plugin.runSelect(query);
 		
 		if (res != null && res.next()) {
@@ -38,9 +56,7 @@ public class DiskControlListener implements Listener{
 			String date = res.getString("date");
 			String hm = res.getString("hm");
 			
-			Bukkit.broadcastMessage(player + " captured the disk on " + date + " at " + hm);
-			
-			//float elapsedTimeMin = date/(60*1000F);
+			p.sendMessage(player + " captured the disk on " + date + " at " + hm);
 		}
 	}
 	
@@ -74,23 +90,19 @@ public class DiskControlListener implements Listener{
 		
 		if(a.equals(Action.RIGHT_CLICK_BLOCK) && b.getType().toString().equals("JUKEBOX") && p.getItemInHand().getType().toString().equals("RECORD_9")){
 			//Check to see if item placed as part of interaction was a RECORD_9
-			//World..getWorld().playEffect(p.getLocation(),"",1);
 			p.getPlayer().getWorld().createExplosion(p.getLocation(), 0);
 			event.getPlayer().getWorld().playEffect(p.getLocation(), Effect.SMOKE, 0);
 			event.getPlayer().getWorld().playEffect(p.getLocation(), Effect.ENDER_SIGNAL, 0);
+			p.getPlayer().getWorld().strikeLightningEffect(p.getPlayer().getTargetBlock(null, 200).getLocation());
 			
 			//Run query to see if last insert is the Same name as the person dropping disk off.
-			
 			String query = "SELECT `id`, `player` FROM capture_info ORDER BY `id` desc LIMIT 1";
-			//ResultSet result = this.plugin.runQuery(query);
-			
 			ResultSet res = this.plugin.runSelect(query);
 			
 			if (res != null && res.next()) {
-				String playern = res.getString("player").toLowerCase();
-				String playerd = p.getDisplayName();
-				if(playern.toLowerCase() != playerd.toString().toLowerCase()){
-					Bukkit.broadcastMessage("'"+playerd+"' == '"+playern+"'");
+				String playerdb = res.getString("player").toLowerCase();
+				String player = p.getDisplayName();
+				if(!(player.equals(playerdb))){
 					query = "INSERT INTO capture_info(`player`, `date`) VALUES('"+p.getDisplayName()+"', NOW());";
 					Bukkit.broadcastMessage(p.getDisplayName() + " has captured the Disk!");
 					this.plugin.runInsert(query);
